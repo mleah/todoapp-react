372c47f22a410c32c56356bf3f8b4d59e72b8c02
Mariah Hanson <mariah.hanson@detroitlabs.com>
2016-10-04 14:26:30 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var helloWorld = <div>Hello world!</div>;
+
+ReactDOM.render(helloWorld,
+document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: [ 'react' ] }
+      }
+    ]
+  }
+};
