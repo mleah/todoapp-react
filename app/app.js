ba64f05ee9fa685093fff895da142a9bcac16f5c
Mariah Hanson <mariah.hanson@detroitlabs.com>
2016-10-04 21:34:21 -0400

    updatin hellowWorld component to be a form

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-   render: function() {
-       return <div>Hello {this.props.name}</div>
-   }
+
+
+var ToDoForm = React.createClass({
+    getInitialState: function() {
+        return {listItem: ''}
+    },
+    render: function(){
+        return (
+            <form>
+                <input type='text' ref='item' value={this.state.listItem}/>
+                <input type='submit' value='Add'/>
+            </form>
+        );
+    }
 });
 
 ReactDOM.render(
-    <HelloWorld name="World"/>,
+    <ToDoForm/>,
 document.getElementById('app'));
