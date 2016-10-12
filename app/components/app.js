0e1fa5cd4e64acbdee17caf2d0136b3dffe3fb1f
Mariah Hanson <mariah.hanson@detroitlabs.com>
2016-10-12 14:42:18 -0400

    adding in some boilerplate for the filter components

--- app/components/app.js
+++ app/components/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ToDoListContainer from './todoListContainer.js'
 import AddItemFormContainer from './addItemFormContainer.js'
 import SortTodoForm from './sortTodoForm.js'
+import Footer from './filter.js'
 
 const App = () => (
     <div className="appContainer">
         <AddItemFormContainer />
         <ToDoListContainer />
         <SortTodoForm />
+        <Footer />
     </div>
 );
 
