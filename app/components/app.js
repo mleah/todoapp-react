7e0749f492657a3de96e96432c68fb43bb93a8b7
Mariah Hanson <mariah.hanson@detroitlabs.com>
2016-10-13 11:33:52 -0400

    adding material-ui into the project and setting up the demo component

--- app/components/app.js
+++ app/components/app.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import ToDoListContainer from './todoListContainer.js'
 import AddItemFormContainer from './addItemFormContainer.js'
 import SortTodoForm from './sortTodo.js'
 import FilterTodoForm from './filterToDo.js'
 
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 const App = () => (
-    <div className="appContainer">
-        <AddItemFormContainer />
-        <ToDoListContainer />
-        <SortTodoForm />
-        <FilterTodoForm />
-    </div>
+
+    <MuiThemeProvider>
+        <div className="appContainer">
+            <AddItemFormContainer />
+            <ToDoListContainer />
+            <SortTodoForm />
+            <FilterTodoForm />
+            <MyAwesomeReactComponent/>
+        </div>
+    </MuiThemeProvider>
 );
 
 export default App
