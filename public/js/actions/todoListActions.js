f282b0b02013afed29dece0d454ad8216557fec3
Mariah Hanson <mariah.hanson@detroitlabs.com>
2016-10-21 14:40:41 -0400

    refactoring the todo actions following DRY principles

--- public/js/actions/todoListActions.js
+++ public/js/actions/todoListActions.js
@@ -5,45 +5,8 @@ export const RECEIVE_TODO_LIST = 'RECEIVE_TODO_LIST';
 export const FAILURE_TODO_LIST = 'FAILURE_TODO_LIST';
 
 
-export function toggleIsFetching(todoList) {
-    return {
-        type: TOGGLE_IS_FETCHING,
-        todoList}
-}
-
-export function receiveToDoList(jsonResponse) {
-    return {
-        type: RECEIVE_TODO_LIST,
-        listItems: jsonResponse.todos,
-        receivedAt: Date.now()
-    }
-}
-
-export function failureToDoList(error) {
-    return {
-        type: FAILURE_TODO_LIST,
-        error: error
-
-    }
-}
-
 export function fetchToDoList() {
-
-    return function (dispatch) {
-
-        dispatch(toggleIsFetching());
-
-        return fetch('http://localhost:3000/api')
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                dispatch(receiveToDoList(json))
-            })
-            .catch(function(error){
-                dispatch(failureToDoList(error));
-            });
-    }
+    return apiCallThunk(fetch('http://localhost:3000/api'));
 }
 
 
@@ -57,44 +20,41 @@ export function addTodo(text, dueDate) {
         completedOn: false
     };
 
-    return function (dispatch) {
-
-        dispatch(toggleIsFetching());
-
-        return fetch('http://localhost:3000/api', {
+    return apiCallThunk(fetch('http://localhost:3000/api', {
             method: 'POST',
             headers: new Headers({
                 'Content-Type': 'text/json'
             }),
             body: JSON.stringify(newTodo)
         })
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                dispatch(receiveToDoList(json));
-            })
-            .catch(function(error){
-                dispatch(failureToDoList(error));
-            });
-    }
+    );
 }
 
 
 export function toggleTodo(id) {
 
+    return apiCallThunk(fetch('http://localhost:3000/api/todo/' + id + '/toggleComplete', {method: 'PUT'}));
+}
+
+
+export function deleteTodo(id) {
+
+    return apiCallThunk(fetch('http://localhost:3000/api/todo/' + id , {method: 'DELETE'}));
+}
+
+
+export function apiCallThunk(fetchCall) {
+
     return function (dispatch) {
 
         dispatch(toggleIsFetching());
 
-        return fetch('http://localhost:3000/api/todo/' + id + '/toggleComplete', {
-            method: 'PUT'
-        })
+        return fetchCall
             .then(function(response) {
                 return response.json();
             })
             .then(function(json) {
-                dispatch(receiveToDoList(json));
+                dispatch(receiveToDoList(json))
             })
             .catch(function(error){
                 dispatch(failureToDoList(error));
@@ -103,24 +63,27 @@ export function toggleTodo(id) {
 }
 
 
-export function deleteTodo(id) {
+export function toggleIsFetching(todoList) {
+    return {
+        type: TOGGLE_IS_FETCHING,
+        todoList}
+}
 
-    return function (dispatch) {
 
-        dispatch(toggleIsFetching());
+export function receiveToDoList(jsonResponse) {
+    return {
+        type: RECEIVE_TODO_LIST,
+        listItems: jsonResponse.todos,
+        receivedAt: Date.now()
+    }
+}
 
-        return fetch('http://localhost:3000/api/todo/' + id , {
-            method: 'DELETE'
-            })
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                dispatch(receiveToDoList(json));
-            })
-            .catch(function(error){
-                dispatch(failureToDoList(error));
-            });
+
+export function failureToDoList(error) {
+    return {
+        type: FAILURE_TODO_LIST,
+        error: error
+
     }
 }
 
